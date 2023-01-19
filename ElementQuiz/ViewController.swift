09cacd55483e43792582f520aaa478e6ed5b77d0
Imani Ward <idward712@gmail.com>
2023-01-19 13:58:31 -0500

    o

--- ElementQuiz/ViewController.swift
+++ ElementQuiz/ViewController.swift
@@ -11,9 +11,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateElement()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var answerLabel: UILabel!
+    
+    let elementList = ["Carbon", "Gold", "Chlorine", "Sodium"]
+    var currentElement = 0
+    var currentElementIndex = 0
+    
+    func updateElement() {
+        let elementName = elementList[currentElementIndex]
+        let image = UIImage(named: elementName)
+        imageView.image = image
+        answerLabel.text = "?"
+    }
 }
 
